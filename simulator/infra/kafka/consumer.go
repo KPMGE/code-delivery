82e01e30652a51eb6b9e9ddf6b6636ea5f3a8ac4
KPMGE <jesuskevin254@gmail.com>
2023-02-26 14:50:32 -0300

    :sparkles: implement kafka consumer

---
+++ simulator/infra/kafka/consumer.go
@@ -0,0 +1,37 @@
+package kafka
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type KafkaConsumer struct {
+	MsgChan chan *ckafka.Message
+}
+
+func (k *KafkaConsumer) Consume() {
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
+		"group.id":          os.Getenv("KAFKA_CONSUMER_GROUP_ID"),
+	}
+
+	c, err := ckafka.NewConsumer(configMap)
+	if err != nil {
+		log.Fatalf("error consuming kafka message: %v", err.Error())
+	}
+
+	topics := []string{os.Getenv("KAFKA_READ_TOPIC")}
+  c.SubscribeTopics(topics, nil)
+
+  fmt.Println("kafka consumer has been started")
+
+  for {
+    msg, err := c.ReadMessage(-1)
+    if err != nil {
+      k.MsgChan <- msg
+    }
+  }
+}
