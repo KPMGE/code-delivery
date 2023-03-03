fc4fb841e9269ad201044526337f96a2c31fd8a6
KPMGE <jesuskevin254@gmail.com>
2023-03-02 22:07:10 -0300

    :sparkles: implement route schema

--- api/src/routes/entities/route.entity.ts
+++ api/src/routes/entities/route.entity.ts
@@ -1,1 +1,28 @@
-export class Route {}
+import { Prop, raw, Schema, SchemaFactory } from "@nestjs/mongoose"
+import { Document } from "mongoose"
+
+export type RouteDocument = Route & Document
+
+@Schema()
+export class Route {
+  @Prop()
+  title: string
+
+  @Prop(
+    raw({
+      lat: Number,
+      lng: Number
+    }),
+  )
+  startPositon: { lat: number; lng: number }
+
+  @Prop(
+    raw({
+      lat: Number,
+      lng: Number
+    }),
+  )
+  endPosition: { lat: number; lng: number }
+}
+
+export const RouteSchema = SchemaFactory.createForClass(Route)
